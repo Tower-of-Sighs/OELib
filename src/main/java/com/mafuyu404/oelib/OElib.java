60d41210de750b949b763d46ff8e6e29285c32dc
Flechazo <2558755403@qq.com>
2025-07-30 17:59:45 +0800

    开挖掘机x2

--- src/main/java/com/mafuyu404/oelib/OElib.java
+++ src/main/java/com/mafuyu404/oelib/OElib.java
@@ -14,6 +14,6 @@ public class OElib implements ModInitializer {
     @Override
     public void onInitialize() {
         DataRegistry.initialize();
-        NetworkHandler.register();
+        NetworkHandler.registerServer();
     }
 }
--- src/main/java/com/mafuyu404/oelib/network/NetworkHandler.java
+++ src/main/java/com/mafuyu404/oelib/network/NetworkHandler.java
@@ -1,8 +1,8 @@
 package com.mafuyu404.oelib.network;
 
 import com.mafuyu404.oelib.OElib;
-import io.netty.buffer.Unpooled;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
@@ -12,20 +12,30 @@ public class NetworkHandler {
 
     public static final ResourceLocation DATA_SYNC_CHUNK_PACKET = new ResourceLocation(OElib.MODID, "data_sync_chunk");
 
-    public static void register() {
+    public static void registerClient() {
         ClientPlayNetworking.registerGlobalReceiver(
                 DATA_SYNC_CHUNK_PACKET,
                 (client, handler, buf, responseSender) -> {
                     DataSyncChunkPacket packet = DataSyncChunkPacket.decode(buf);
                     client.execute(() -> DataSyncChunkPacket.handle(packet));
                 });
+    }
+
+    public static void registerServer() {
+        ServerPlayNetworking.registerGlobalReceiver(
+                DATA_SYNC_CHUNK_PACKET,
+                (server, player, handler, buf, responseSender) -> {
+                    DataSyncChunkPacket packet = DataSyncChunkPacket.decode(buf);
+                    server.execute(() -> DataSyncChunkPacket.handle(packet));
+                });
         OElib.LOGGER.info("Registered network packets for data synchronization");
     }
 
 
     public static void sendTo(ServerPlayer player, DataSyncChunkPacket packet) {
-        FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
+        FriendlyByteBuf buf = PacketByteBufs.create();
         packet.encode(buf);
         ServerPlayNetworking.send(player, NetworkHandler.DATA_SYNC_CHUNK_PACKET, buf);
     }
+
 }
