75189aed5f590f971a44da8a4b7f4469d53adb79
Flechazo <2558755403@qq.com>
2025-08-14 15:34:24 +0800

    修复小bug

---
+++ forge/src/main/java/com/mafuyu404/oelib/forge/OELibForgeClient.java
@@ -0,0 +1,18 @@
+package com.mafuyu404.oelib.forge;
+
+import com.mafuyu404.oelib.OELib;
+import com.mafuyu404.oelib.api.net.NetworkManager;
+import com.mafuyu404.oelib.forge.data.net.DataSyncChunkPacket;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = OELib.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class OELibForgeClient {
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        NetworkManager.registerPackets(DataSyncChunkPacket.class);
+    }
+}
